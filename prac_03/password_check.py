27e4edfcae698ea50187e89c7350541e275100bc
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-08-27 14:35:07 +1000

    Complete password_check.py

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,29 @@
 """
-Password Length Checker Program
+Password Length Checker Program (modified)
 """
 
 MINIMUM_PASSWORD_LENGTH = 10
 
-password = input("Password: ")
-while len(password) < MINIMUM_PASSWORD_LENGTH:
-    print("Password too short, must be at least 10 characters long")
+
+def main():
+    """Checks password"""
+    password = get_password()
+    while len(password) < MINIMUM_PASSWORD_LENGTH:
+        print("Password too short, must be at least 10 characters long")
+        password = get_password()
+    print_number_of_asterisks(password)
+
+
+def print_number_of_asterisks(password):
+    """Prints an asterisk for each character in the password"""
+    for _ in password:
+        print("*", end="")
+
+
+def get_password():
+    """Gets password from user"""
     password = input("Password: ")
-for character in password:
-    print("*", end="")
+    return password
+
+
+main()
