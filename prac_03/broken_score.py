c12cde31345a00a1d61f650e5629e82345f845ff
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-08-28 15:38:54 +1000

    Complete broken_score.py

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,16 +1,31 @@
 """
 CP1404 - Practical
-Fixed program to determine score status
+Fixed program to determine score status do-over
 """
 
-score = float(input("Enter score: "))
-while score != -1:
-    if score > 100 or score < 0:
-        print("Invalid score")
-    elif score > 90:
-        print("Excellent")
-    elif score > 50:
-        print("Passable")
-    else:
-        print("Bad")
+import random
+
+
+def main():
+    """Ask user for score and prints the results & repeats with a random score"""
     score = float(input("Enter score: "))
+    result = determine_score_result(score)
+    print(result)
+    random_score = random.randint(0, 101)
+    random_result = determine_score_result(random_score)
+    print(random_result)
+
+
+def determine_score_result(score):
+    """Determines what result the score gets and returns answer"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
