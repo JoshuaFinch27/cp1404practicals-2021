136fa7faea0e0a4ea854c2c8a5c38c0d60e66a74
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-09-20 15:05:23 +1000

    Add progress update on guitars.py, error checking not working, will attempt to remove it after this commit

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,6 +4,10 @@ Cp1404 prac_06 - Guitar Program - uses Guitar class
 from prac_06.guitar import Guitar
 
 PROGRAM_DESCRIPTION = "My guitars!"
+CURRENT_YEAR = 2021
+GUITAR_TOO_OLD = 1000
+MAXIMUM_GUITAR_COST = 3000000  # Most expensive guitar sold for $2,700,000 https://www.gak.co.uk/blog/expensive-guitars/
+MINIMUM_GUITAR_COST = 0
 
 
 def main():
@@ -12,18 +16,18 @@ def main():
     print(PROGRAM_DESCRIPTION)
     name = input("Name: ")
     while name != "":
-        year =
-
-
-def verify_user_input(prompt, data_type):
-    """Get and verify user input based on variable parameters"""
-    if data_type == "any":
-        user_input = input(prompt)
-        while user_input == "" or user_input == " ":
-            print("Invalid input.")
-            user_input = input(prompt)
-    else:
-        user_input = data_type(input(prompt))
+        year = int(input("Year: "))
+        while CURRENT_YEAR < year < GUITAR_TOO_OLD:
+            print("Invalid year of construction/purchase.")
+            year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        while MAXIMUM_GUITAR_COST < cost <= MINIMUM_GUITAR_COST:
+            print(f"Invalid cost of guitar, must be more than ${MINIMUM_GUITAR_COST} and less than ${MAXIMUM_GUITAR_COST}")
+            cost = float(input("Cost: "))
+        add_new_guitar = Guitar(name, year, cost)
+        guitars.append(add_new_guitar)
+        print(f"{add_new_guitar} added.\n")
+        name = input("Name: ")
 
 
 main()
