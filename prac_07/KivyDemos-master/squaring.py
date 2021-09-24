8e9dd7c5622095a3634f5b5d22cb1c5c2634285b
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-09-24 13:22:25 +1000

    Add fixed docstring for squaring.py to represent the fact that it has been modified by me (according to prac instructions)

--- prac_07/KivyDemos-master/squaring.py
+++ prac_07/KivyDemos-master/squaring.py
@@ -1,17 +1,13 @@
 """
-CP1404/CP5632 Practical
+CP1404 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
+
 """
 
-# import ...  ???
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-
-__author__ = 'Lindsay Ward'
 
 
 class SquareNumberApp(App):
