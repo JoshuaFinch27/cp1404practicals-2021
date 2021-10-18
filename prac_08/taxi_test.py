38881d77e111b716cd9efa47d3910c4655a58d3a
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-10-18 10:46:43 +1000

    Add updated taxi.py and taxi_test.py

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -7,7 +7,7 @@ from prac_08.taxi import Taxi
 
 def main():
     """Test Taxi class (inherited from Car class)"""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
